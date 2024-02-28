5950f0ab5a62a6f250892359d24a85eb9900315f
10053465 <ravi.dhiman@thalesgroup.com>
2024-02-28 18:22:26 +0530

    adding titles

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import pandas
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(layout="wide")
 
@@ -16,6 +18,15 @@ with col2:
 content2 = """
 Below you can find the apps I have built in python. Feel Free to contact me."""
 st.write(content2)
+
+col3, col4 = st.columns(2)
+df = pandas.read_csv("data.csv", sep=";")
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
 
     
 
